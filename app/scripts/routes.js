59cd71d845aa28d85969531454a6d5e716945808
Holly Moody <hollymoody@charter.net>
2014-12-12 18:54:54 -0500

    Modify routes config factory for inline annotation needed for grunt build minification.

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,27 +1,32 @@
-angular.module('MovnThereUI').config(function($routeProvider) {
-    'use strict';
+angular.module('MovnThereUI')
+    .config( [
+        '$routeProvider',
+        function($routeProvider) {
+            'use strict';
 
-    $routeProvider
-        .when('/', {
-            templateUrl: 'templates/home.html',
-            controller: 'HomeCtrl'
-        })
-        .when('/users', {
-            templateUrl: 'templates/users.html',
-            controller: 'UserCtrl'
-        })
-        .when('/login', {
-            templateUrl: 'templates/login.html',
-            controller: 'LoginCtrl'
-        })
-        .when('/signup', {
-            templateUrl: 'templates/signup.html',
-            controller: 'SignupCtrl'
-        })
-        .when('/logout', {
-            controller: 'LogoutCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeCtrl'
+                })
+                .when('/users', {
+                    templateUrl: 'templates/users.html',
+                    controller: 'UserCtrl'
+                })
+                .when('/login', {
+                    templateUrl: 'templates/login.html',
+                    controller: 'LoginCtrl'
+                })
+                .when('/signup', {
+                    templateUrl: 'templates/signup.html',
+                    controller: 'SignupCtrl'
+                })
+                .when('/logout', {
+                    controller: 'LogoutCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    ]
+);
