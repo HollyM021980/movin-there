73a117e7a770f56c5e6bb8e61f0862b3b0ce0a06
Holly Moody <hollymoody@charter.net>
2014-12-12 17:48:07 -0500

    Modify logout controller for inline annotation needed for grunt build minification.

--- app/scripts/controllers/logout.controller.js
+++ app/scripts/controllers/logout.controller.js
@@ -1,9 +1,16 @@
-angular.module('MovnThereUI').controller('LogoutCtrl', function($scope, $location, AuthFactory) {
-  'use strict';
+angular.module('MovnThereUI')
+  .controller('LogoutCtrl', [
+    '$scope',
+    '$location',
+    'AuthFactory',
+    function($scope, $location, AuthFactory) {
+      'use strict';
 
-  $scope.logout = function(credentials) {
-    AuthFactory.login(credentials).success(function(response) {
-      $location.path('/');
-    });
-  };
-});
+      $scope.logout = function(credentials) {
+        AuthFactory.login(credentials).success(function(response) {
+          $location.path('/');
+        });
+      };
+    }
+  ]
+);
