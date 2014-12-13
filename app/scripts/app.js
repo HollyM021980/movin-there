5057420e070a43eae91305b5de4be92aae29798a
Holly Moody <hollymoody@charter.net>
2014-12-12 19:01:24 -0500

    Modify app.js for inline annotation needed for grunt build minification.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,35 @@
 // initialize the app
-angular.module('MovnThereUI',[
-  'ngRoute'
-]).run(function(
-    $rootScope,
-    $location,
-    $http,
-    $window,
-    AuthFactory,
-    UserFactory
-) {
-    'use strict';
-    $rootScope.$on('$routeChangeStart', function(event, next) {
+angular.module('MovnThereUI',['ngRoute'])
+    .run([
+        '$rootScope',
+        '$location',
+        '$http',
+        '$window',
+        'AuthFactory',
+        'UserFactory',
+        function(
+        $rootScope,
+        $location,
+        $http,
+        $window,
+        AuthFactory,
+        UserFactory
+        ) {
+            'use strict';
+            $rootScope.$on('$routeChangeStart', function(event, next) {
 
-        if (AuthFactory.isAuthenticated()) {
-            if (AuthFactory.isAuthenticated()) {
-                $http.defaults.headers.common['Authorization'] = 'Token token=' + $window.sessionStorage.getItem('movnThereUI.user');
-            }
+                if (AuthFactory.isAuthenticated()) {
+                    if (AuthFactory.isAuthenticated()) {
+                        $http.defaults.headers.common['Authorization'] = 'Token token=' + $window.sessionStorage.getItem('movnThereUI.user');
+                    }
 
-            UserFactory.fetch();
-        } else {
-            if (($location.path() !== '/login') && ($location.path() !== '/signup')) {
-                $location.path('/login');
-            }
+                    UserFactory.fetch();
+                } else {
+                    if (($location.path() !== '/login') && ($location.path() !== '/signup')) {
+                        $location.path('/login');
+                    }
+                }
+            });
         }
-    });
-});
+    ]
+);
