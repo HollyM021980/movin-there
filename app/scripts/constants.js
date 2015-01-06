c53ac419c920e642e7c8f29e6623a5404bf19d73
Holly Moody <hollymoody@charter.net>
2015-01-06 17:20:22 -0500

    Zip radius mostly working.  Returns 200 but async call returns 404.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,5 +1,6 @@
 angular.module('MovnThereUI')
 .constant('_', window._)
 .constant('GglKey', 'AIzaSyDauT5fcf7PjOB7Kp3aM4xua9GBub1Q724')
+.constant('ZipKey', 'mR7CGwyCmaENF3yKGeaiO33FaCQg7cNt3V6KrEnOO2Bu5aeMpo5ds8TCKJTSWHm0')
 .constant('ServerUrl', 'https://movn-there-api.herokuapp.com/');
 // .constant('ServerUrl', 'http://localhost:3000/');
--- app/scripts/controllers/templates/point-info.controller.js
+++ app/scripts/controllers/templates/point-info.controller.js
@@ -2,7 +2,7 @@ angular.module('MovnThereUI')
 .controller('PointInfoCtrl', function ($scope) {
   $scope.templateValue = 'hello from the template itself';
   $scope.clickedButtonInWindow = function () {
-    var msg = 'clicked a window in the template!';
+    var msg = 'Future: display place and census data';
     alert(msg);
   }
 });
